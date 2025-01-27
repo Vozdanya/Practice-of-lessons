b9f7b444eb3d35b5e9e547847c9f0e9f8f7b3cd4
Vozdanya <156104348+Vozdanya@users.noreply.github.com>
2025-01-28 04:20:06 +0500

    migration of file "rooms"

--- src/models/rooms.py
+++ src/models/rooms.py
@@ -4,7 +4,7 @@ from sqlalchemy import String, ForeignKey
 from src.database import Base
 
 
-class HotelsOrm(Base):
+class RoomsOrm(Base):
     __tablename__ = 'rooms'
 
     id: Mapped[int] = mapped_column(primary_key=True)
